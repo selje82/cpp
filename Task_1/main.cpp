b1144d3d78ed20472e83ad47c5eee64254228a82
Glenn Seljeseth <selje82@hotmail.com>
2026-06-12 19:24:31 +0200

    Just structured the learning path in some folders.

---
+++ Task_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+    std::cout << "What is your name?" << std::endl;
+    std::getline(std::cin, name);
+    
+    std::cout << "Your name is " << name << "!\n" << std::endl;
+
+    return 0;
+    
+}
