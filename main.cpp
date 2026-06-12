1af708695269d2e3461faebe1e35aa771fe9e2e4
Glenn Seljeseth <selje82@hotmail.com>
2026-06-12 18:50:13 +0200

    Added my first function, just to greet a person with name and age.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
 
+void greet(std::string name, int age) {
+    std::cout << "Hello " << name << "." << "You are " << age << " years old." << std::endl;
+}
+
 int main() {
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::getline(std::cin, name); 
-    std::cout << "Hello, " << name << "!\n" << std::endl;
+    
+    greet("Glenn", 43);
     return 0;
 }
