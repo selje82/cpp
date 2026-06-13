c2ed7110feaa7b28593f6ad753d79c645d01d074
Glenn Seljeseth <selje82@hotmail.com>
2026-06-13 15:55:11 +0200

    Made a converter from Celsius to Fahrenheit degrees with using if/else if/ else statements.

---
+++ Task_2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+    
+    // Declaring variables for Celsius and Fahrenheit. 
+    double tempCelsius;
+    double tempFahrenheit;
+
+    // Asking user for input in Celsius
+    std::cout << "Input a degree in Celsius:" << std::endl;
+    std::cin >> tempCelsius;
+
+    // Converting Celsius to Fahrenheit.
+    tempFahrenheit = (tempCelsius * 1.8) + 32;
+
+    // Checking the temperature, and outputting different results based on if/else if/else statements. 
+    if (tempFahrenheit <= 32) {
+        std::cout << "It's freezing outside, remember your coat. The temperature is: " << tempFahrenheit << " Fahrenheit degrees." << std::endl;
+    } else if (tempFahrenheit >= 86) {
+        std::cout << "It's a hot day today. Remember suncream. The temperature is: " << tempFahrenheit << " Fahrenheit degrees." << std::endl;
+    } else {
+        std::cout << "The temperature today is: " << tempFahrenheit << " Fahrenheit degrees." << std::endl;
+    }
+    return 0;
+
+}
